2bf7526ba6b6cdf290a39ede5ccb3906122cd294
acer <acer@10.0.0.111>
2023-10-05 16:37:10 +0530

    first commit

---
+++ project98/src/com/tnif/inheritance/SingleDemo.java
@@ -0,0 +1,12 @@
+package com.tnif.inheritance;
+
+public class SingleDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Student st=new Student("divya",3456,"png",98765);
+		System.out.println(st);
+
+	}
+
+}
