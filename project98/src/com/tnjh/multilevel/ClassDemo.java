48e4ea78a487f9ebfa406c4a14abc3e8719265f3
acer <acer@10.0.0.111>
2023-10-05 17:10:07 +0530

    first commit

---
+++ project98/src/com/tnjh/multilevel/ClassDemo.java
@@ -0,0 +1,9 @@
+package com.tnjh.multilevel;
+
+public class ClassDemo {
+	public static void main(String[] args) {
+		Collegestudent sg=new Collegestudent("satya",4500);
+		System.out.println(sg);
+
+}
+}
