60aae1ba41304fde85c50b18a2b7a6a3d124200f
acer <acer@10.0.0.111>
2023-10-05 16:13:40 +0530

    first commit

---
+++ project98/src/com/iuyg/encapsulation/EncapsulaationDemo.java
@@ -0,0 +1,16 @@
+package com.iuyg.encapsulation;
+
+public class EncapsulaationDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Encapsulation ob=new Encapsulation();//create object
+		ob.serialno=25;//accessing the data member by using dot operator
+		ob.name="divya sai";
+		ob.age=21;
+		ob.show();
+
+
+	}
+
+}
---
+++ project98/src/com/iuyg/encapsulation/Encapsulation.java
@@ -0,0 +1,13 @@
+package com.iuyg.encapsulation;
+
+public class Encapsulation {
+	public int serialno;
+	public String name;
+	public int age;
+	//member function
+	public void show() {
+System.out.println("serialno"+serialno+"\t"+"name"+name+"\t"+"age"+age);		
+
+
+}
+}
