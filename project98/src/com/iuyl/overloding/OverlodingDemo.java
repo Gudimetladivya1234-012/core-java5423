c4d0758ac0b42b8c8b3568576921ca842a58cc4a
acer <acer@10.0.0.111>
2023-10-05 16:21:34 +0530

    first commit

---
+++ project98/src/com/iuyl/overloding/OverlodingDemo.java
@@ -0,0 +1,14 @@
+package com.iuyl.overloding;
+
+public class OverlodingDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+	Point p1=new Point(9.8f);
+	System.out.println(p1);
+	Point p2=new Point(9.8f,9.8f);
+	System.out.println(p2);
+
+	}
+
+}
