a67c3f4085b1fff56c72faf90f84481985aeacb2
acer <acer@10.0.0.111>
2023-10-05 15:29:51 +0530

    first commit

---
+++ project34/src/com/tndif/helloworld/Helloworld.java
@@ -0,0 +1,11 @@
+package com.tndif.helloworld;
+
+public class Helloworld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Helloworld");
+
+	}
+
+}
