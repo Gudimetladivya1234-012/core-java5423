12251638046a3f407b8386ccc6642aad956bf9ae
acer <acer@10.0.0.109>
2023-10-07 14:54:41 +0530

    first commit

--- project98/src/com/yujh/markerinterface/Demo.java
+++ project98/src/com/yujh/markerinterface/Demo.java
@@ -1,5 +1,14 @@
 package com.yujh.markerinterface;
-
 public class Demo {
 
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Student s=new Student(1,"satya");
+		if(s instanceof Regestarable)
+		System.out.println("student registered");
+		else
+			System.out.println("student not registered");
+
+	}
 }
+
