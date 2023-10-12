613416c61a6fdb6e40b0b2aea4dcb3a5cdcd8ee3
acer <acer@10.0.0.105>
2023-10-12 14:17:22 +0530

    first commit

---
+++ project98/src/com/kjiu/Static/EmployeeDemo.java
@@ -0,0 +1,16 @@
+package com.kjiu.Static;
+
+
+	public class EmployeeDemo {
+
+		public static void main(String[] args) {
+			// TODO Auto-generated method stub
+	Employee ob=new Employee("divya",777);
+	System.out.println(ob);
+	Employee companyName;//accessing the static variable using  class name
+
+		}
+
+	}
+
+
