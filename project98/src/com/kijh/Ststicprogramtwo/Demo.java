796a6e1420d30a294c5dd2df6147387f2c62ab48
acer <acer@10.0.0.105>
2023-10-12 14:25:31 +0530

    first commit

---
+++ project98/src/com/kijh/Ststicprogramtwo/Demo.java
@@ -0,0 +1,17 @@
+package com.kijh.Ststicprogramtwo;
+
+public class Demo {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Myclass.display();//static mathod
+		Myclass obb1=new Myclass();
+		System.out.println("obb1");
+		Myclass obb2=new Myclass();
+		System.out.println("obb2");
+
+
+	}
+
+}
+
+
---
+++ project98/src/com/kijh/Ststicprogramtwo/Myclass.java
@@ -0,0 +1,30 @@
+package com.kijh.Ststicprogramtwo;
+
+public class Myclass {
+	
+		private int section;//instance variable
+		private static int srNo;//static variable
+		static {
+			System.out.println("------staticblock------");
+			srNo=1000;
+		}
+		//default constructor
+		Myclass(){
+			System.out.println("------default consructor------");
+			srNo++;
+			section++;
+		}
+		//static method
+		static void display() {
+			//	System.out.println("section"+section);
+			System.out.println("serialno"+srNo);
+		}
+		@Override
+		public String toString() {
+			return "Myclass [section=" + section + "]";
+		}
+		
+
+	}
+
+
