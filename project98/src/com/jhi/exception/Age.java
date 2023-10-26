a5b3accf08ac2c0f7862a97a81e0903ab36873d1
acer <acer@10.0.0.113>
2023-10-26 14:36:15 +0530

    first commit

---
+++ project98/src/com/jhi/exception/Age.java
@@ -0,0 +1,6 @@
+package com.jhi.exception;
+public class Age extends Exception {
+  Age(String str){
+	System.out.println(str);
+	}
+}
---
+++ project98/src/com/jhi/exception/UserDefined.java
@@ -0,0 +1,19 @@
+package com.jhi.exception;
+
+
+
+import java.util.Scanner;
+
+public class UserDefined {
+	   private static int age;
+	   static void validate() throws Age{
+		Scanner sc=new Scanner(System.in);
+		System.out.println("enter age");
+		age=sc.nextInt();
+		if (age<18)
+	       throw new Age("invalid age");
+		else
+			System.out.println("valid age");
+		   
+	   }
+	}
