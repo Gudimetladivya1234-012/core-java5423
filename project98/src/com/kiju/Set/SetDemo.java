bce6f0cb2ba91916ed7726a26b8322d5d96d9f1f
acer <acer@192.168.1.51>
2023-11-01 14:36:46 +0530

    first commit

---
+++ project98/src/com/kiju/Set/SetDemo.java
@@ -0,0 +1,12 @@
+package com.kiju.Set;
+
+public class SetDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//SetOperation.operations();
+		TreeSetProg.treeSetOperations();
+
+	}
+
+}
